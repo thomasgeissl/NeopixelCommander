ed6e8bef6e4f02c8bf8989bbb9fb712543d00adf
Thomas Geissl <thomas.geissl@gmail.com>
2026-01-12 00:02:20 +0100

    added 8x8 and 5x5 example

--- examples/example-5x5/example-5x5.ino
+++ examples/example-5x5/example-5x5.ino
@@ -5,6 +5,7 @@ NeopixelCommander *neopixelCommander = nullptr;
 void setup()
 {
   Serial.begin(115200);
+  delay(5000);
 
   //generate a deterministic ssid and password
   //based on mac address, persistent accross reboots and very likely not conflicting with other devices in the same range
@@ -14,16 +15,16 @@ void setup()
   auto pw_100 = ((seed / 81) % 8) + 1;
 
   auto pw = pw_100 * 100 + pw_10 * 10 + pw_1 * 1;
-  Serial.println("pw:");
-  Serial.println(pw);
   String ssidString = "neopixels-";
   ssidString += pw;
 
   String passwordString = "password-";
   passwordString += pw;
 
-  // neopixelCommander = new NeopixelCommander("how-soon-is-now?", "a!!mylov!n", 2, 64, 127);
-  neopixelCommander = new NeopixelCommander(ssidString.c_str(), passwordString.c_str(), 2, 64, 127);
+  Serial.print("pw:");
+  Serial.println(passwordString);
+
+  neopixelCommander = new NeopixelCommander(ssidString.c_str(), passwordString.c_str(), 2, 25, 255);
 
   neopixelCommander->begin();
   neopixelCommander->setUseFallbackCredentialsAfterBootInactivity(30000);
---
+++ examples/example-8x8/example-8x8.ino
@@ -0,0 +1,37 @@
+#include <NeopixelCommander.h>
+
+NeopixelCommander *neopixelCommander = nullptr;
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(5000);
+
+  //generate a deterministic ssid and password
+  //based on mac address, persistent accross reboots and very likely not conflicting with other devices in the same range
+  auto seed = macToNumber();
+  auto pw_1 = (seed % 8) + 1;
+  auto pw_10 = ((seed / 9) % 8) + 1;
+  auto pw_100 = ((seed / 81) % 8) + 1;
+
+  auto pw = pw_100 * 100 + pw_10 * 10 + pw_1 * 1;
+  String ssidString = "neopixels-";
+  ssidString += pw;
+
+  String passwordString = "password-";
+  passwordString += pw;
+
+  Serial.print("pw:");
+  Serial.println(passwordString);
+
+  neopixelCommander = new NeopixelCommander(ssidString.c_str(), passwordString.c_str(), 2, 64, 127);
+
+  neopixelCommander->begin();
+  neopixelCommander->setUseFallbackCredentialsAfterBootInactivity(30000);
+  neopixelCommander->setExecuteStoredCodeAfterBootInactivity(60000);
+}
+
+void loop()
+{
+  neopixelCommander->loop();
+}
